7fb2c28ce11d4170feb5a8e45041534906006981
Zsargul <kubakerlino@gmail.com>
2022-08-15 22:03:58 +0100

    changed config.h

--- config.h
+++ config.h
@@ -3,20 +3,22 @@
 #define TERM "st"
 #define BROWSER "qutebrowser"
 
-/* gaps */
+/* Appearance */
+static const char *fonts[]          = { "Terminus:style=Bold:size=16:autohint=true" };
+static const unsigned int snap      = 32;       /* snap pixel */
+static const int showbar            = 1;     /* 0 means no bar */
+static const int topbar             = 1;     /* 0 means bottom bar */
+static const char dmenufont[]       = "Terminus:style=Bold:size=16:autohint=true";
+
+/* Gaps & Border */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 30;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-
-/* appearance */
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Terminus:style=Bold:size=16:autohint=true" };
-static const char dmenufont[]       = "Terminus:style=Bold:size=16:autohint=true";
+
+/* Colors */
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -28,7 +30,7 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
-/* tagging */
+/* Tags */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
@@ -44,7 +46,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
@@ -121,27 +123,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 
 	/* Exit DWM */
-	{ MODKEY|ShiftMask,             XK_F24,    quit,           {1} },                                                                // Kill DWM with exit code 1
-																	 //
-	/* Gap related keybinds */
-	/*
-	{ MODKEY|Mod4Mask,              XK_u,      incrgaps,       {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_i,      incrigaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_i,      incrigaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_o,      incrogaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_o,      incrogaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_6,      incrihgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_6,      incrihgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_7,      incrivgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_7,      incrivgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_8,      incrohgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_9,      incrovgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
-	*/
+	{ MODKEY|ShiftMask,             XK_F24,    quit,           {1} }, 																	 
 																	 
 	/* Tags */
 	TAGKEYS(                        XK_1,                      0)
@@ -155,9 +137,28 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 
 	/* Unused commands. Defined with 'NULL' as key and cast to long unsigned int to avoid build warnings. */
-	{ MODKEY,                       (long unsigned int)NULL,      togglebar,      {0} },                                             // Toggle bar 
+	{ MODKEY,                       (long unsigned int)NULL,      togglebar,      {0} },
 	{ MODKEY,                       (long unsigned int)NULL,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       (long unsigned int)NULL,      zoom,           {0} },
+	
+	/* Gap related keybinds */
+	{ MODKEY|Mod4Mask,              (long unsigned int)NULL,      incrgaps,       {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    (long unsigned int)NULL,      incrgaps,       {.i = -1 } },
+	{ MODKEY|Mod4Mask,              (long unsigned int)NULL,      incrigaps,      {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    (long unsigned int)NULL,      incrigaps,      {.i = -1 } },
+	{ MODKEY|Mod4Mask,              (long unsigned int)NULL,      incrogaps,      {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    (long unsigned int)NULL,      incrogaps,      {.i = -1 } },
+	{ MODKEY|Mod4Mask,              (long unsigned int)NULL,      incrihgaps,     {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    (long unsigned int)NULL,      incrihgaps,     {.i = -1 } },
+	{ MODKEY|Mod4Mask,              (long unsigned int)NULL,      incrivgaps,     {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    (long unsigned int)NULL,      incrivgaps,     {.i = -1 } },
+	{ MODKEY|Mod4Mask,              (long unsigned int)NULL,      incrohgaps,     {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    (long unsigned int)NULL,      incrohgaps,     {.i = -1 } },
+	{ MODKEY|Mod4Mask,              (long unsigned int)NULL,      incrovgaps,     {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    (long unsigned int)NULL,      incrovgaps,     {.i = -1 } },
+	{ MODKEY|Mod4Mask,              (long unsigned int)NULL,      togglegaps,     {0} },
+	{ MODKEY|Mod4Mask|ShiftMask,    (long unsigned int)NULL,      defaultgaps,    {0} },
+
 };
 
 /* button definitions */
