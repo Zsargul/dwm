a488c2fe4658a40b1197613030e6c266dbaae1f2
Zsargul <kubakerlino@gmail.com>
2022-09-25 17:33:47 +0100

    changed config

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const char *fonts[]          = {
 	"Terminus:style=Bold:size=16:autohint=true",
 	"Noto Color Emoji:pixelsize=16:antialias=true:autohint=true"
 };
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 5;       /* snap pixel */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
 static const char dmenufont[]       = "Terminus:style=Bold:size=16:autohint=true";
@@ -43,9 +43,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class                  instance    title             tags mask     isfloating   monitor */
+	{ "Gimp",                 NULL,       NULL,             0,            1,           -1 },
+	{ "Firefox",              NULL,       NULL,             1 << 8,       0,           -1 },
+	{ "TelegramDesktop",      NULL,       "Telegram",       0,            1,           -1 },
 };
 
 /* layout(s) */
