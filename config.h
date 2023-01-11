c71716ecfbd753a86031b033c5caf5462e88ba8a
Zsargul <kubakerlino@gmail.com>
2023-01-11 16:54:19 +0000

    added center patch

--- config.h
+++ config.h
@@ -43,12 +43,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class                              instance    title             tags mask     isfloating   monitor */
-	{ "Gimp",                             NULL,       NULL,             0,            1,           -1 },
-	{ "Firefox",                          NULL,       NULL,             1 << 8,       0,           -1 },
-	{ "TelegramDesktop",                  NULL,       "Telegram",       0,            1,           -1 },
-	{ "net-runelite-launcher-Launcher",   NULL,       NULL,             0,            1,           -1 },
-	{ "st-256color",                      NULL,       "st",             0,            1,           -1 },
+	/* class                              instance    title             tags mask     iscentered   isfloating   monitor */
+	{ "Gimp",                             NULL,       NULL,             0,            0,           1,           -1 },
+	{ "Firefox",                          NULL,       NULL,             1 << 8,       0,           0,           -1 },
+	{ "st-256color",                      NULL,       "st",             0,            1,           1,           -1 },
 };
 
 /* layout(s) */
