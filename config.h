79a36a3a94f276c8a7b6288f98036994a0c3170e
Zsargul <kubakerlino@gmail.com>
2023-01-11 19:26:36 +0000

    added dimensions rule to st

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 #include <X11/XF86keysym.h>
 
-#define TERM "st"
+#define TERM "st -g 100x30"
 #define BROWSER "qutebrowser"
 
 /* Appearance */
@@ -44,7 +44,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class                              instance    title             tags mask     iscentered   isfloating   monitor */
-	{ "Gimp",                             NULL,       NULL,             0,            0,           1,           -1 },
+	{ "Gimp",                             NULL,       NULL,             0,            0,           0,           -1 },
 	{ "Firefox",                          NULL,       NULL,             1 << 8,       0,           0,           -1 },
 	{ "st-256color",                      NULL,       "st",             0,            1,           1,           -1 },
 };
